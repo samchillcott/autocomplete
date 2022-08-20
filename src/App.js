b68134c340733b3081275d01a28a13c2c79f93f1
samchillcott1 <samchillcott@gmail.com>
2022-08-20 10:26:51 +0100

    add useEffect fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    const loadUsers = async () => {
+      const response = await axios.get()
+      console.log(response.data);
+      setUsers(response.data.data)
+    }
+    loadUsers()
+  }, [])
+
   return (
     <div className="App">
       <input type="text" />
