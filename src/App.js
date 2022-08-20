d5d10ea4497e39564e5b0012e4e9f6982081e624
samchillcott1 <samchillcott@gmail.com>
2022-08-20 10:44:29 +0100

    configure handler

--- src/App.js
+++ src/App.js
@@ -4,18 +4,32 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [users, setUsers] = useState([])
+  const [text, setText] = useState("")
+  const [suggestions, setSuggestions] = useState([])
   useEffect(() => {
     const loadUsers = async () => {
       const response = await axios.get('https://reqres.in/api/users?page=2')
-      console.log(response.data);
       setUsers(response.data.data)
     }
     loadUsers()
   }, [])
 
+  const onChangeHandler = (text) => {
+    let matches = []
+    if (text.length > 0) {
+      matches = users.filter(user => {
+        const regex = new RegExp(`${text}`, "gi");
+        return user.email.match(regex)
+      })
+    }
+    console.log(matches);
+    setSuggestions(matches)
+    setText(text)
+  }
+
   return (
     <div className="App">
-      <input type="text" />
+      <input type="text" onChange={ e => onChangeHandler(e.target.value) } value={ text } />
     </div>
   );
 }
